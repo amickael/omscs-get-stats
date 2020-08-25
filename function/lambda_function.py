75169b271f4d0385db80d6e691d7a9c27551b315
Andrew Mickael <andrew.mickael@tdn2k.com>
2020-08-24 19:07:27 -0500

    Added code

---
+++ function/lambda_function.py
@@ -0,0 +1,53 @@
+import os
+import time
+from decimal import Decimal
+from typing import Any
+import json
+import boto3
+from boto3.dynamodb.conditions import Key
+
+try:
+    import dotenv
+
+    dotenv.load_dotenv("../.env")
+except ImportError:
+    pass
+
+
+########################################################################################################################
+# Configuration
+########################################################################################################################
+DYNAMODB_TABLE = os.getenv("DYNAMODB_TABLE")
+
+
+########################################################################################################################
+# Configuration
+########################################################################################################################
+class BetterJSONEncoder(json.JSONEncoder):
+    def default(self, o: Any) -> Any:
+        if isinstance(o, Decimal):
+            return int(o)
+        else:
+            return super().default(o)
+
+
+########################################################################################################################
+# Application
+########################################################################################################################
+def lambda_handler(event: dict, context):
+    dynamodb = boto3.resource("dynamodb")
+    table = dynamodb.Table(DYNAMODB_TABLE)
+    response = table.query(
+        Select="ALL_ATTRIBUTES",
+        ScanIndexForward=False,
+        KeyConditionExpression=Key(os.getenv("DYNAMODB_KEY")).eq(
+            os.getenv("MATRICULATION")
+        )
+        & Key(os.getenv("DYNAMODB_SORT")).gte(int(int(time.time() * 1000) - 4.32e8)),
+    )
+    items = response["Items"]
+    return {
+        "statusCode": 200,
+        "body": json.dumps(items, cls=BetterJSONEncoder),
+        "contentType": "application/json",
+    }
